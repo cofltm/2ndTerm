6123bd05d9830e316c49072981c86121dc2aa0b4
Cherish Sarmiento <111546406+cofltm@users.noreply.github.com>
2023-02-23 15:19:29 +0800

    Create wonyoung.java

---
+++ week11/wonyoung.java
@@ -0,0 +1,31 @@
+package gui;
+
+import javax.swing.JFrame;
+import javax.swing.ImageIcon;
+import javax.swing.JLabel;
+import java.awt.Image;
+import java.awt.FlowLayout;
+
+public class wonyoung {
+    JFrame frame = new JFrame();
+    ImageIcon bg = new ImageIcon("ive-jang-wonyoung-vogue.jpg");
+    
+    public wonyoung(){
+        Image img = bg.getImage();
+        bg = new ImageIcon(img.getScaledInstance(1600, 767, Image.SCALE_SMOOTH)); 
+        frame.setSize(1600, 767);
+        frame.setDefaultCloseOperation(frame.EXIT_ON_CLOSE);
+        
+        JLabel lblBack = new JLabel(bg);
+        lblBack.setLayout(new FlowLayout());
+        lblBack.setBounds(0, 0, 1600, 767);
+        frame.add(lblBack); 
+                
+        frame.setVisible(true);
+        
+    }
+    public static void main(String [] args){
+        new wonyoung();
+        
+    }
+}
