9fc89fa70d02ea4e8fca79683bd11224fbfcbb96
Cherish Sarmiento <111546406+cofltm@users.noreply.github.com>
2023-02-23 15:26:12 +0800

    Update wonyoung.java

--- week11/wonyoung.java
+++ week11/wonyoung.java
@@ -5,10 +5,12 @@ import javax.swing.ImageIcon;
 import javax.swing.JLabel;
 import java.awt.Image;
 import java.awt.FlowLayout;
+import javax.swing.JButton;
 
 public class wonyoung {
     JFrame frame = new JFrame();
     ImageIcon bg = new ImageIcon("ive-jang-wonyoung-vogue.jpg");
+    JButton btn = new JButton("Login");
     
     public wonyoung(){
         Image img = bg.getImage();
@@ -19,6 +21,10 @@ public class wonyoung {
         JLabel lblBack = new JLabel(bg);
         lblBack.setLayout(new FlowLayout());
         lblBack.setBounds(0, 0, 1600, 767);
+        
+        btn.setBounds(100, 100, 100, 100);
+        lblBack.add(btn);
+        
         frame.add(lblBack); 
                 
         frame.setVisible(true);
@@ -29,3 +35,4 @@ public class wonyoung {
         
     }
 }
+
